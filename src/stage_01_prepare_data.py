86ac9f12d9c80e335f30846520fc6e0e4ad9b721
Lalit <lt72@rediffmail.com>
2022-06-22 16:20:34 +0530

    data prep added

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -1,9 +1,12 @@
 import argparse
+from concurrent.futures import process
+from encodings import utf_8
 import os
 import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_mgmt import process_posts
 import random
 
 
@@ -21,7 +24,31 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+
+    source_data_dir= config['source_data']['data_dir']
+    source_data_file=config['sourct_data']['data_file']
+    source_data_path=os.path.join(source_data_dir, source_data_file)
+
+    split=params['prepare']['split']#split ration
+    seed=params['prepare']['seed']
+
+    random.seed(seed)
+
+    artifacts=config['artifacts']
+    prepare_data_dir_path=os.join(artifacts['ARTIFACTS_DIR'],artifacts['PREPARED_DATA'])
+    create_directories(['prepare_data_dir_path'])
+
+    train_data_path=os.path.join(prepare_data_dir_path,artifacts['TRAIN_DATA'])
+    test_data_path=os.path.join(prepare_data_dir_path,artifacts['TEST_DATA'])
+
+    encode='utf_8'
+
+    with open(source_data_path, encoding=encode) as fd_in: #Actual input data
+        with open(train_data_path, 'w', encoding=encode) as fd_out_train: #writing train data
+            with open(test_data_path, 'w', encoding=encode) as fd_out_test: #wrotomg test data
+                process_posts(fd_in, fd_out_train, fd_out_test,'<python>',split)
+
+
 
 
 if __name__ == '__main__':
