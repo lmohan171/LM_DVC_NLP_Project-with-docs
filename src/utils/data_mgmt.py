35c4343e16870834c66f103057eaeac2a1884d2f
Lalit <lt72@rediffmail.com>
2022-06-23 20:11:46 +0530

    try exception added

--- src/utils/data_mgmt.py
+++ src/utils/data_mgmt.py
@@ -14,13 +14,14 @@ def process_posts(fd_in, fd_out_train, fd_out_test,target_tag,split):
 
             pid=attr.get('ID', '')
             label= 1 if target_tag in attr.get('Tags','') else 0
-            title=re.sub(r'\s+','', attr.get('Title','')).strip()
-            body=re.sub(r'\s+','', attr.get('Body','')).strip()
-            text=f'{title} {body}'#title+""+body
+            title=re.sub(r'\s+',' ', attr.get('Title','')).strip()
+            body=re.sub(r'\s+',' ', attr.get('Body','')).strip()
+            text=f'{title} {body}'#title+" "+body
 
             fd_out.write(f'{pid}\t{label}\t{text}\n')
             line_num+=1
 
         except Exception as e:
-            msg=f"Skipping the broken lines{line_num}:{e}"
+            msg=f"Skipping the broken lines{line_num}:{e}\n"
+            logging.exception(msg)
 
