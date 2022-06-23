1645c95bdcbff667518e2d14462905f2737dc2ea
Lalit <lt72@rediffmail.com>
2022-06-23 18:32:10 +0530

    minor change

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -42,11 +42,11 @@ def main(config_path, params_path):
     train_data_path=os.path.join(prepare_data_dir_path,artifacts['TRAIN_DATA'])
     test_data_path=os.path.join(prepare_data_dir_path,artifacts['TEST_DATA'])
 
-    encode='utf_8'
+    encode='utf8'
 
     with open(source_data_path, encoding=encode) as fd_in: #Actual input data
         with open(train_data_path, 'w', encoding=encode) as fd_out_train: #writing train data
-            with open(test_data_path, 'w', encoding=encode) as fd_out_test: #wrotomg test data
+            with open(test_data_path, 'w', encoding=encode) as fd_out_test: #writing test data
                 process_posts(fd_in, fd_out_train, fd_out_test,'tag',split)
 
 
