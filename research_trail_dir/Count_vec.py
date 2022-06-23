2d35f35a58fcc416ed78691c28ff1b9116b5ee14
Lalit <lt72@rediffmail.com>
2022-06-23 23:04:11 +0530

    new details added

--- research_trail_dir/Count_vec.py
+++ research_trail_dir/Count_vec.py
@@ -1,11 +1,12 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
 corpus=[
-    'zebr apple ball cat',
+    'apple ball cat',
     'ball cat dog elephant',
     'very very unizque'
 ]
 
 vectorizer= CountVectorizer()
 X=vectorizer.fit_Transform(corpus)
-print(X)
+print(X.toarray())
+print(vectorizer.get_feature_name_out())
