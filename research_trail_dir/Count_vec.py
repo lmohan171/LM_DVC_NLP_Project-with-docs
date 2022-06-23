451315780079ab42bb55e95705a3944bbb38baf1
Lalit <lt72@rediffmail.com>
2022-06-23 23:40:47 +0530

    minor edit

--- research_trail_dir/Count_vec.py
+++ research_trail_dir/Count_vec.py
@@ -6,7 +6,10 @@ corpus=[
     'very very unizque'
 ]
 
-vectorizer= CountVectorizer()
-X=vectorizer.fit_Transform(corpus)
+max_features=100
+ngrams=2 #trigram
+
+vectorizer= CountVectorizer(max_features=max_features,ngram_range=(1,ngrams))
+X=vectorizer.fit_transform(corpus)
 print(X.toarray())
-print(vectorizer.get_feature_name_out())
+print(vectorizer.get_feature_names_out())
