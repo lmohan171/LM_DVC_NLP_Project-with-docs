e510e051cf2fbb45d6c592bb2989e6b05e858549
Lalit <lt72@rediffmail.com>
2022-06-24 16:03:14 +0530

    minor change

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -5,7 +5,7 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories, process_posts
+from src.utils import read_yaml, create_directories, process_posts
 import random
 
 
