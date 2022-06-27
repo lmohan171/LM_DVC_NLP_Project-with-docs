5a9e07b71f89d84154a0216998afce207f8b4240
Lalit <lt72@rediffmail.com>
2022-06-28 00:03:59 +0530

    minor change

--- src/stage_03_training.py
+++ src/stage_03_training.py
@@ -40,7 +40,7 @@ def main(config_path, params_path):
 
     matrix=joblib.load(featurized_train_data_path)
 
-    labels=np.squeeze(matrix[:1].toarray())
+    labels=np.squeeze(matrix[:,1].toarray())
     X=matrix[:,2:]
 
     logging.info(f"input_matrix_size:{matrix.shape}")
@@ -60,6 +60,7 @@ def main(config_path, params_path):
         n_jobs=n_jobs,
         random_state=seed
     )
+
     model.fit(X,labels)
 
     joblib.dump(model, model_path)
